2098615032854c3e5484dc0d6dc89a0ef1e9fbea
Davut <davutsenol.fr@gmail.com>
2026-06-29 00:50:15 +0200

    fix(video): use aspect-[9/16] container with object-cover fill

--- src/components/VideoPlayer.tsx
+++ src/components/VideoPlayer.tsx
@@ -6,8 +6,8 @@ interface Props {
 
 export default function VideoPlayer({ videoUrl }: Props) {
   return (
-    <div className="rounded-xl overflow-hidden bg-black">
-      <video src={videoUrl} controls autoPlay loop className="w-full aspect-video object-contain" />
+    <div className="rounded-xl overflow-hidden bg-black aspect-[9/16]">
+      <video src={videoUrl} controls autoPlay loop className="w-full h-full object-cover" />
     </div>
   )
 }
